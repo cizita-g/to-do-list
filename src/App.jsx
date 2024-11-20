a0e71a4ba0c32f5cf0f1f772823c1057307dbdd4
Cizita Gautam <cizita.gautam06@gmail.com>
2024-11-20 23:06:08 +0545

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,52 @@
-import { useState } from "react"
-import "./style.css"
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import "./styles.css"
+import { TodoList } from "./TodoList"
 
-export default function App (){
-  const[newItem, setnewItem] = useState("")
-  //setnewItem ('sfdjgh')
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
 
+    return JSON.parse(localValue)
+  })
 
-  return ( 
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
     <>
-  <form className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="new-item">New Item</label>
-      <input
-       value ={newItem} 
-       onChange= {e => setnewItem(e.target.value)}
-       type="text" 
-       id="item"
-     />
-    </div>
-
-    <button className="btn">Add Task</button>
-
-  </form>
-
-   <h1 className="header">Todo List</h1>
-   <ul className="list">
-    <li>
-      <label htmlFor="">
-        <input type="checkbox" />
-        Item 1
-      </label>
-       <button className="btn-btn-danger">Delete</button>
-
-    </li>
-
-    <li>
-      <label htmlFor="">
-        <input type="checkbox" />
-        Item 2
-      </label>
-       <button className="btn-btn-danger">Delete</button>
-
-    </li>
-
-
-
-   </ul>
-
-
-
-  </>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
   )
 }
-
